cf84d19b2deec024d918d54a6b141db5ecc70ed4
Gabtwotwo <Gabtwotwo@github.com>
2020-12-08 15:52:44 -0500

    Fixed new players loading

--- Assets/Scripts/GameSaver.cs
+++ Assets/Scripts/GameSaver.cs
@@ -21,9 +21,14 @@ public class GameSaver : MonoBehaviour
 
     public void Load()
     {
-     
-       
-        
+
+        if(PlayerPrefs.GetFloat("X Position") == 0.0f && PlayerPrefs.GetFloat("Y Position") == 0.0f)
+        {
+            Debug.Log("Initial load!");
+            PlayerPrefs.SetFloat("X position", -14.4f);
+            PlayerPrefs.SetFloat("Y position", 11.2f);
+        }
+
         Debug.Log("Loaded!");
         transform.position = new Vector3(PlayerPrefs.GetFloat("X position"), PlayerPrefs.GetFloat("Y position"), 6.0f);
     } 
