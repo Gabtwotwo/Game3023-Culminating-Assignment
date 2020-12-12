7c59719546a3d1520853da729e7aa56814905269
Gabtwotwo <Gabtwotwo@github.com>
2020-12-11 19:23:16 -0500

    added respawn button just in case

---
+++ Assets/RespawnButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnButtonBehaviour : MonoBehaviour
+{
+
+    public GameObject playerReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnRespawnButtonClicked()
+    {
+        playerReference.transform.position = new Vector3(-11.43f, -15.38f, 6.39f);
+    }
+}
